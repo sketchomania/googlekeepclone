c0f583bf9369891bc45d2a0801cfcfb2276a313a
Vaibhav Kushwaha <vaibhavvast6529@gmail.com>
2021-12-03 17:54:37 +0530

    Initialized React

---
+++ web/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import MainMenu from "../components/label/MainMenu";
+import MainArea from "../components/main/MainArea";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Ypu're in home page</h1>
+      <MainMenu />
+      <MainArea />
+    </div>
+  );
+};
+
+export default HomePage;
