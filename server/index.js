3e06c950af593ceac03d63af36c7ab4687d90dad
Vaibhav Kushwaha <vaibhavvast6529@gmail.com>
2022-04-18 09:07:04 +0530

    Added dotenv

--- server/index.js
+++ server/index.js
@@ -1,23 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
 
-// import noteRoutes from "./routes/notes.js";
-// import labelRoutes from "./routes/labels.js";
-
 import graphQLSchema from "./graphql/schema/index.js";
 import graphQLResolvers from "./graphql/resolvers/index.js";
 
 const app = express();
 
+const HOSTUSER = process.env.DB_USER;
+const HOSTPASS = process.env.DB_PASS;
+const HOSTNAME = process.env.DB_NAME;
+const PORT = process.env.PORT;
+
 app.use(bodyParser.json({ limit: "20mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
 app.use(cors());
-
-// app.use("/notes", noteRoutes);
-// app.use("/labels", labelRoutes);
 
 app.use(
   "/graphql",
@@ -28,9 +30,8 @@ app.use(
   })
 );
 
-const CONNECTION_URL =
-  "mongodb+srv://clipclip:Rezgs0bT5Ge7uNi4@cluster0.nylpb.mongodb.net/g-keep?retryWrites=true&w=majority";
-const port = process.env.PORT || 5000;
+const CONNECTION_URL = `mongodb+srv://${HOSTUSER}:${HOSTPASS}@cluster0.nylpb.mongodb.net/${HOSTNAME}?retryWrites=true&w=majority`;
+const port = PORT || 6000;
 
 mongoose
   .connect(CONNECTION_URL, {
@@ -41,8 +42,3 @@ mongoose
     app.listen(port, () => console.log(`Server running on port: ${port}`))
   )
   .catch((error) => console.log(error));
-
-// mongoose.set("useFindAndModify", false);
-// clipclip
-// "mongodb+srv://googlekeeep:googlekeep007@cluster0.nylpb.mongodb.net/g-keep?retryWrites=true&w=majority";
-   
