39ac9e055958f09934b2f3c70939db7df748b9ac
Vaibhav Kushwaha <vaibhavvast6529@gmail.com>
2022-12-14 22:46:26 +0530

    feat: setting card and user card in appBar

--- web/src/components/appbar/AppBar.jsx
+++ web/src/components/appbar/AppBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -25,6 +26,9 @@ const AppBar = ({ darkMode, toggleTheme, toggleLabelMenu }) => {
   const labelLoading = useSelector((state) => state.labelReducer.isLoading);
   const noteLoading = useSelector((state) => state.noteReducer.isLoading);
   const dispatch = useDispatch();
+
+  const [showSettingCard, setShowSettingCard] = useState(false);
+  const [showUserCard, setShowUserCard] = useState(false);
 
   const logo = (
     <img
@@ -99,33 +103,123 @@ const AppBar = ({ darkMode, toggleTheme, toggleLabelMenu }) => {
                 <View className={styles.iconStyleBig} />
               )}
             </li>
-            <li title="Setting">
-              <Setting className={`${styles.iconStyleBig}`} />
-              {/* <Refresh className={`${styles.iconStyleBig}`} /> */}
-              {/* <Spinner /> */}
+            <li className="relative">
+              <Setting
+                title="Setting"
+                className={`${styles.iconStyleBig}`}
+                onClick={() => {
+                  console.log("user Backdrop chilcked");
+                  setShowSettingCard(true);
+                }}
+              />
+              {showSettingCard && (
+                <>
+                  <div
+                    className="fixed h-full w-full top-0 left-0"
+                    onClick={() => {
+                      console.log("Setting Backdrop chilcked");
+                      setShowSettingCard(false);
+                    }}
+                  ></div>
+                  <div
+                    className="absolute flex flex-col justify-around shadow-md shadow-zinc-400 border-t border-t-zinc-100 text-zinc-700 text-sm py-1.5 w-max bg-white rounded-lg"
+                    onClick={() => {
+                      console.log("Settings Modal clicked");
+                    }}
+                  >
+                    <h3 className="hover:bg-zinc-200 pl-5 pr-3 py-1 cursor-pointer">
+                      Settings
+                    </h3>
+                    <h3
+                      className="hover:bg-zinc-200 pl-5 pr-3 py-1 cursor-pointer"
+                      onClick={toggleTheme}
+                    >
+                      Enable dark theme
+                    </h3>
+                    <h3 className="hover:bg-zinc-200 pl-5 pr-3 py-1 cursor-pointer">
+                      Send feedback
+                    </h3>
+                    <h3 className="hover:bg-zinc-200 pl-5 pr-3 py-1 cursor-pointer">
+                      Help
+                    </h3>
+                    <h3 className="hover:bg-zinc-200 pl-5 pr-3 py-1 cursor-pointer">
+                      App downloads
+                    </h3>
+                    <h3 className="hover:bg-zinc-200 pl-5 pr-3 py-1 cursor-pointer">
+                      Keyboard shortcuts
+                    </h3>
+                  </div>
+                </>
+              )}
             </li>
           </ul>
         </div>
-        <div className={`${container}`}>
+        <div className={`${container} mr-4`}>
           <ul className={`${ulSty}`}>
             <li>
-              {/* <Github title="Github" className={`${styles.iconStyleBig}`} /> */}
-              <Account
-                title={auth ? auth.user.email : "User"}
-                className={styles.iconStyleBig}
-                onClick={() => {
-                  console.log("👤: ", auth);
-                }}
-              />
+              <Github title="Github" className={`${styles.iconStyleBig}`} />
             </li>
-            {/* </ul>
 
-          <ul className={`${ulSty}`}> */}
-            <li>
+            <li className="relative" title={`User`}>
               {auth.token && (
-                <button title="Logout" onClick={logoutHandler}>
-                  <Logout className={styles.iconStyleBig} />
-                </button>
+                <Account
+                  // title={auth ? auth.user.email : "User"}
+                  className={`${styles.iconStyleBig}`}
+                  onClick={() => {
+                    setShowUserCard(true);
+                    // console.log("👤: ", auth);
+                  }}
+                />
+              )}
+              {showUserCard && (
+                <>
+                  <div
+                    className="fixed h-full w-full top-0 left-0"
+                    onClick={() => {
+                      console.log("user Backdrop chilcked");
+                      setShowUserCard(false);
+                    }}
+                  ></div>
+                  <div
+                    className={`absolute flex flex-col items-center justify-around right-0 w-80 bg-white rounded-lg
+                     shadow-lg shadow-zinc-400 border-t border-t-zinc-200 text-zinc-700  text-sm
+                    `}
+                    onClick={() => {
+                      console.log("user Modal clicked");
+                    }}
+                  >
+                    <h3
+                      className="mt-8 font-semibold text-zinc-500 scale-90"
+                      onClick={() => {
+                        console.log("👤: ", auth);
+                        setShowUserCard(true);
+                      }}
+                    >
+                      {auth.user.email}
+                    </h3>
+                    <h3 className="cursor-pointer border rounded-full font-semibold py-1 px-4 my-5">
+                      {"Manage your Google Account"}
+                    </h3>
+                    <h3 className="border-y w-full font-semibold py-2.5 flex items-center pl-16">
+                      {"Add another account"}
+                    </h3>
+                    {/* <Logout
+                      title="Logout"
+                      onClick={logoutHandler}
+                      className={styles.iconStyleBig}
+                    /> */}
+                    <h3
+                      title="Logout"
+                      onClick={logoutHandler}
+                      className="cursor-pointer border rounded-md font-semibold px-6 py-1.5 my-4 hover:bg-zinc-100"
+                    >
+                      {"Sign out"}
+                    </h3>
+                    <h3 className="border-t w-full text-xs py-3.5 flex items-center justify-center">
+                      {"Privacy policy • Terms of services"}
+                    </h3>
+                  </div>
+                </>
               )}
             </li>
           </ul>
